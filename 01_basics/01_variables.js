75aa3f2f5ee7640b6e4f15914a1b32bc27a72757
Glennie678 <139850024+Glennie678@users.noreply.github.com>
2023-09-06 05:22:54 +0000

    container config done1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 55668
+let accountEmail = "alibaba@google"
+var accountPassword = "23456"
+accountCity =" Lahore"
+
+let accountstate;
+
+// accountId =456
+
+accountEmail = "fiazbai"
+accountPassword= "1111"
+accountCity = "pak"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
