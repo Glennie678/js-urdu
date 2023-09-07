b49d22b88169c6a8d0cf8db5d4755703296dffa8
Glennie678 <139850024+Glennie678@users.noreply.github.com>
2023-09-07 01:26:30 +0000

    container config ok

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -7,10 +7,12 @@ let accountstate;
 
 // accountId =456
 
-accountEmail = "fiazbai"
+accountEmail = "Ali"
 accountPassword= "1111"
 accountCity = "pak"
 
 console.log(accountId)
+
+/*prefer not to use var because of issue in block scope and funtional sccope*/
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 +3) //we are using nodejs, not brower
+
+console.log(3 + 3) //code readability should be high
+console.log("Ali")
+
+let name = "Ali"
+let age = 20
+let isLogedIn = false
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/fasle
+// null => standlone value
+//undefined =>
+//symbol =>unique
+
+
+//Object types
+
+console.log(typeof null)// object 
+console.log(typeof undefined) // undefined
