68641786a4ed7f72d1daa09c65f65096a2419db0
Glennie678 <139850024+Glennie678@users.noreply.github.com>
2023-09-08 00:58:28 +0000

    Converions of datatypes

---
+++ 01_basics/03_converionsOperation.js
@@ -0,0 +1,29 @@
+let score = undefined
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33, iss ma ak string value nbr ma chnge kar shkta ha or kr type be nuber ho jy ge.
+//"33abc" => NaN, iss ma string ke type tu chage ho shkt but wo ap ko kbi be ak pure nbr ni show ho ga. 
+// true => 1, iss ma ke type boolen show ho ge or ak nbr show wo 0 or 1 ke form ma ho ga agra ap true kar gy tu 1show ho ga or agra ap false show karya gy tu 0 show ho ga.
+
+//undefined => same as comment 2
+
+let isLoggedIn = 0
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolenIsLoggedIn)
+
+
+// 1=> true, iss ma ak number ko boolen ma change kar shkta ha or ya koe be nbr ho shkta is ma koe limit ni ha ka 0 ya 1 he likhana ha but false tb he show ho ga jb ap 0 likha gy other 2nd condition ma true he show ho ga.
+
+// "" => false, iss ma agra hm empty kar dy tu phir show kary ga otherwise mean jese name linkha tu true show kary ga jese "Ali".
+
+let someNuber = 45
+let stringInNubmer = String(someNuber)
+console.log(stringInNubmer)
+console.log(typeof stringInNubmer)
+
+// 45 =>45, ya nazar ana ma tu ak pure number ha but is ke type ak srting ban gy ha as ma bs ya he change ha 
