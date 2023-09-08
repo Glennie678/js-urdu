854d19aecad38000476bfbc5f0d283fa3bdf4e68
Glennie678 <139850024+Glennie678@users.noreply.github.com>
2023-09-08 02:43:55 +0000

    comparsions

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,35 @@
+// console.log(1>2);
+// console.log(1>=2);
+// console.log(1<2);
+// console.log(1<=2);
+// console.log(1==2);
+// console.log(1!=2);
+
+// easy to understand all these comparsion.
+
+//***problem creating comparsions */
+
+// console.log("2">1);
+// console.log("02">1);   //ya wala method theek mi but Js ma use hota ha as tarha kea ak language ha us ma  strict rules ko follow kay jataa ha us ka name TypeScript ha or wo sir ka 2nd mean English Channel pa us ko bd ma karyaa gy ha jb JS compelet ho jy gee.
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<=0); // equlity and comparsion are two differents things in null type and also its working process are different, now these informartion are enough for basics after some project read it also.
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined<=0); // but here its working is same as it is.
+
+//the comparsion start from line nbr 15 to 23 is confused them when you makie a logica.
+
+// === is mean strict check it checks is type and comparsion it if 0ne value is str and other is nbr thats shows false value.
+
+console.log("2"===2)
+
+
+
+
+
+
